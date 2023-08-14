84982531ff04fe81a496e243496a491e0eb85fcb
Knutmf <knutm-f@hotmail.com>
2023-08-14 04:12:52 +0200

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,8 +1,8 @@
 const app = require('./app'); // Import the configured server
-const PORT = 9000;
+const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => {
 console.log(`Server is running on
-http://localhost:${PORT}`
+http://localhost:${PORT}`|| designated port on the server.
 );
 });
